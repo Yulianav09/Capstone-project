2af1d8877bc67784e1ce4cc6d475ad5d3b99d6e3
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-06-26 20:02:02 -0500

    Added HTML structure

---
+++ script.js
@@ -0,0 +1,12 @@
+// const programArray = []
+const speakersArray = []
+
+// let string = `<article>
+// <div class="imgProgram"><img src="${programArray[i].src}" alt="Program 1"></div>
+// <div class="programCards">
+//   <h3>Lecture</h3>
+//   <p class="programDescription">A daily selection of privately personalized reads; no accounts or sign-ups required.
+//     has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.
+//   </p>
+// </div>
+// </article>`
