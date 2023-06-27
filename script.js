613c5b82cec1acb693b7379632a679b1ba904ce2
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-06-27 14:42:02 -0500

    Added mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 // const programArray = []
-const speakersArray = []
+// const speakersArray = []
+const hamburgerContainer = document.querySelector('#hamburgerContainer');
+const nav = document.querySelector('nav');
+const navigationLinks = document.querySelector('.navigationLinks');
+const hamburgerBar = document.querySelectorAll('.hamburgerBar');
+const closebutton = document.querySelector('#closebutton');
 
+hamburgerContainer.addEventListener('click', () => {
+  if (navigationLinks.classList.contains('onlyDesktop')) {
+    navigationLinks.classList.remove('onlyDesktop');
+    nav.classList.add('navMobile');
+    for (let i = 0; i < hamburgerBar.length; i += 1) {
+      hamburgerBar[i].classList.add('hidden');
+    }
+    closebutton.classList.remove('hidden');
+  } else {
+    nav.classList.remove('navMobile');
+    navigationLinks.classList.add('onlyDesktop');
+    for (let i = 0; i < hamburgerBar.length; i += 1) {
+      hamburgerBar[i].classList.remove('hidden');
+    }
+    closebutton.classList.add('hidden');
+  }
+});
+
+function closewindow() {
+  nav.classList.remove('navMobile');
+  navigationLinks.classList.add('onlyDesktop');
+  for (let i = 0; i < hamburgerBar.length; i += 1) {
+    hamburgerBar[i].classList.remove('hidden');
+  }
+  closebutton.classList.add('hidden');
+}
+
+navigationLinks.addEventListener('click', closewindow);
 // let string = `<article>
 // <div class="imgProgram"><img src="${programArray[i].src}" alt="Program 1"></div>
 // <div class="programCards">
