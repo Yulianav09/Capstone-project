c74a98926d7bb2bfaf8e9862ea0836007db136f1
Yuliana Velez <velezrodriguez2001@gmail.com>
2023-06-29 01:32:34 -0500

    Added JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,88 @@
 // const programArray = []
-// const speakersArray = []
+const featuredSpeakers = [
+  {
+    name: 'Juan Carlos Martínez',
+    profession: 'He is the founder and director of the organization Animal Defenders International (ADI) in Colombia.',
+    description: 'Martinez dedicates to combating the use of animals in shows and promoting animal protection laws.',
+    featureImage: './src/Juan carlos.png',
+  },
+  {
+    name: 'Juan Carlos Martínez',
+    profession: 'He is the founder and director of the organization Animal Defenders International (ADI) in Colombia.',
+    description: 'Martinez dedicates to combating the use of animals in shows and promoting animal protection laws.',
+    featureImage: './src/Juan carlos.png',
+  },
+  {
+    name: 'Juan Carlos Martínez',
+    profession: 'He is the founder and director of the organization Animal Defenders International (ADI) in Colombia.',
+    description: 'Martinez dedicates to combating the use of animals in shows and promoting animal protection laws.',
+    featureImage: './src/Juan carlos.png',
+  },
+  {
+    name: 'Juan Carlos Martínez',
+    profession: 'He is the founder and director of the organization Animal Defenders International (ADI) in Colombia.',
+    description: 'Martinez dedicates to combating the use of animals in shows and promoting animal protection laws.',
+    featureImage: './src/Juan carlos.png',
+  },
+  {
+    name: 'Juan Carlos Martínez',
+    profession: 'He is the founder and director of the organization Animal Defenders International (ADI) in Colombia.',
+    description: 'Martinez dedicates to combating the use of animals in shows and promoting animal protection laws.',
+    featureImage: './src/Juan carlos.png',
+  },
+  {
+    name: 'Juan Carlos Martínez',
+    profession: 'He is the founder and director of the organization Animal Defenders International (ADI) in Colombia.',
+    description: 'Martinez dedicates to combating the use of animals in shows and promoting animal protection laws.',
+    featureImage: './src/Juan carlos.png',
+  }]
 const hamburgerContainer = document.querySelector('#hamburgerContainer');
 const nav = document.querySelector('nav');
 const navigationLinks = document.querySelector('.navigationLinks');
 const hamburgerBar = document.querySelectorAll('.hamburgerBar');
 const closebutton = document.querySelector('#closebutton');
+const defendersContainer = document.querySelector("#defendersContainer");
+const defenderButton = document.querySelector(".defenderButton");
+const homeLink = document.querySelector("#homeLink");
+const aboutLink = document.querySelector("#aboutLink");
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+
+
+featuredSpeakers.forEach((speaker, index) => {
+  const speakerCard= document.createElement('article');
+  speakerCard.classList.add('speakerContainer')
+  if (index > 1) speakerCard.classList.add('onlyDesktop')
+  speakerCard.innerHTML = `
+  <div class="imgdefender"><img src="${speaker.featureImage}" alt="Animal Defender 1"></div>
+  <div class="defendersCards">
+    <h3>${speaker.name}</h3>
+    <p class="defendersProfession">${speaker.profession}</p>
+    <hr>
+    <p class="desfenderDescription">${speaker.description}</p>
+  </div>`
+  defendersContainer.appendChild(speakerCard);
+} )
+
+const hiddenCards = document.querySelectorAll(".speakerContainer.onlyDesktop")
+
+defenderButton.addEventListener('click', () => {
+  hiddenCards.forEach(element => {element.classList.toggle("onlyDesktop")})
+})
+
+homeLink.addEventListener('click', () => {
+  home.classList.remove('hidden')
+  about.classList.add('hidden')
+})
+aboutLink.addEventListener('click', () => {
+  home.classList.add('hidden')
+  about.classList.remove('hidden')
+})
 
 hamburgerContainer.addEventListener('click', () => {
   if (navigationLinks.classList.contains('onlyDesktop')) {
     navigationLinks.classList.remove('onlyDesktop');
+    homeLink.classList.remove('onlyDesktop');
     nav.classList.add('navMobile');
     for (let i = 0; i < hamburgerBar.length; i += 1) {
       hamburgerBar[i].classList.add('hidden');
@@ -17,6 +91,7 @@ hamburgerContainer.addEventListener('click', () => {
   } else {
     nav.classList.remove('navMobile');
     navigationLinks.classList.add('onlyDesktop');
+    homeLink.classList.add('onlyDesktop');
     for (let i = 0; i < hamburgerBar.length; i += 1) {
       hamburgerBar[i].classList.remove('hidden');
     }
@@ -34,12 +109,3 @@ function closewindow() {
 }
 
 navigationLinks.addEventListener('click', closewindow);
-// let string = `<article>
-// <div class="imgProgram"><img src="${programArray[i].src}" alt="Program 1"></div>
-// <div class="programCards">
-//   <h3>Lecture</h3>
-//   <p class="programDescription">A daily selection of privately personalized reads; no accounts or sign-ups required.
-//     has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.
-//   </p>
-// </div>
-// </article>`
